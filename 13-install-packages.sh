4a54abe26c856efe6187b6c61a32d33666c61d57
Potu Nagamani <Nagamani.X.Potu@kp.org>
2024-12-19 08:53:24 +0530

    added 2nd script

--- 13-install-packages.sh
+++ 13-install-packages.sh
@@ -3,7 +3,22 @@
 ID=$(id -u)
 R="\e[31m"
 G="\e[32m"
+Y="\e[33m"
 N="\e[0m"
+
+TIMESTAMP=$(date +F%-%H-%M-%S)
+LOGFILE=LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+echo "script started executing at $TIMESTAMP" &>> $LOGFILE
+
+VALIDATE(){
+    if [$1 -ne 0]
+    then 
+       echo -e "$2...$R failed $N"
+    else
+        echo -e "$2...$G success $N"
+    fi
+}
 
 if [ $ID -ne 0 ] 
 then  
@@ -13,4 +28,16 @@ else
     echo "you are root user"
 fi
 
-echo "All args passed :: $@"
+#echo "All args passed: $@"
+
+for $package in $@
+do 
+  yum list installed $package &>> $LOGFILE
+  if ($? -ne o)
+  then
+    yum install $package -y  &>> $LOGFILE
+    VALIDATE $? "installation of $package"
+    else
+        echo -e "$package is already installed ....$Y skipping $N"
+    fi
+done
