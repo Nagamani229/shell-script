1e2daf8230e0f44f149ac25457eeff43291678a3
Potu Nagamani <Nagamani.X.Potu@kp.org>
2024-12-21 14:07:23 +0530

    shell-script

--- 15-delete-old-logs.sh
+++ 15-delete-old-logs.sh
@@ -12,6 +12,14 @@ then
     echo -e "$R Source directory: $SOURCE_DIR does not exists. $N"
 fi
 
+mkdir -p /tmp/shellscript-logs
+
+if [ -d "/tmp/shellscript-logs" ]; then
+    find /tmp/shellscript-logs -name "*.log" -delete
+else
+    echo "Directory /tmp/shellscript-logs does not exist."
+fi
+
 FILES_TO_DELETE=$(find $SOURCE_DIR -type f -mtime +14 -name "*.log")
 
 while IFS= read -r line
