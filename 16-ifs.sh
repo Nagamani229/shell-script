768db6c6c339c5798e73fad112465fa91875ca84
Potu Nagamani <Nagamani.X.Potu@kp.org>
2024-12-21 14:28:17 +0530

    shell-script

---
+++ 16-ifs.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+file=/etc/passwd
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+if [ ! -f $file ]
+then 
+    echo -e "$R source directory: $file does not exists. $N"
+fi
+
+while IFS= ":" read -r username passwd user-id user_fullname group_id home_dir
+do
+   echo "username: $username"
+   echo "user ID: $userid"
+   echo "user full name: $userfullname
+done < $file
