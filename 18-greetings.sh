7ffbb18df5b1b5a398e282b8f56cc150c0af81d4
Potu Nagamani <Nagamani.X.Potu@kp.org>
2024-12-21 20:09:08 +0530

    shell-script

--- 18-greetings.sh
+++ 18-greetings.sh
@@ -4,31 +4,29 @@ NAME=""
 WISHES="Good Morning"
 
 USAGE(){
-    echo "USAGE:: $(basename $0) -n <Name> -w <wishes>"
-    echo "options;:"
-    echo "-n, specify the name(mandatory)"
-    echo "-w, specify the wishes.(optional)" default is Good Morning
-    echo " -h, display help and exit"
-
+    echo "USGAE:: $(basename $0) -n <name> -w <wishes>"
+    echo "Options::"
+    echo " -n, Specify the name (mandatory)"
+    echo " -w, Specify the wishes. (Optional). Default is Good Morning"
+    echo " -h, Display Help and exit"
 }
 
 while getopts ":n:w:h" opt; do
-  case $opt in 
-   n) NAME="$OPTARG";;
-   w) WISHES="$OPTARG";;
-   \?) echo "Invalid options: -"$OPTARG""  >&2; USAGE; exit;;
-   :) USAGE; exit;;
-   h) USAGE; exit;;
-  esac
-
+    case $opt in
+        n) NAME="$OPTARG";;
+        w) WISHES="$OPTARG";;
+        \?) echo "invalid options: -"$OPTARG"" >&2; USAGE; exit;;
+        :) USAGE; exit;;
+        h) USAGE; exit;;
+    esac
 done
 
-  #if [ -z "$NAME" ] || [ -z "$WISHES" ]; then
-if [ -z "$NAME" ]
-  #echo "ERROR: Both -n and -w are mandatory options"
-  echo "ERROR: -n is mandatory"
-  USAGE
-  exit 1
+#if [ -z "$NAME" ] || [ -z "$WISHES" ]; then
+if [ -z "$NAME" ]; then # now wishes is optional
+    #echo "ERROR: Both -n and -w are mandatory options."
+    echo "ERROR: -n is mandatory."
+    USAGE
+    exit 1
 fi
 
- echo "Hello $NAME. $WISHES. I have been learning shell script"
+echo "Hello $NAME. $WISHES. I have been learning Shell Script."
