fc4d6525860c3e93bbed26bbb8362f2776e36cff
Potu Nagamani <Nagamani.X.Potu@kp.org>
2024-12-21 19:44:04 +0530

    shell-script

--- 18-greetings.sh
+++ 18-greetings.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
-PERSON=$1
+NAME=""
+WISHES=""
 
-echo "Hello $PERSON, Good Morning. We are learning shell script"
+USAGE(){
+    echo "USAGE:: $(basename $0) -n <Name> -w <wishes>"
+    echo "options;:"
+    echo "-n, specify the name(mandatory)"
+    echo "-w, specify the wishes.ex, Good Morning"
+    echo " -h, display help and exit"
+
+}
+
+while getopts ":n:w:h" opt; do
+  case $opt in 
+   n) NAME="$OPTARG";;
+   w) WISHES="$OPTARG";;
+   h|*) USAGE; exit;;
+  esac
+
+done
