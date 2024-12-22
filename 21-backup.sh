0308468b4a8c71d31f429f77237b343894123be6
Potu Nagamani <Nagamani.X.Potu@kp.org>
2024-12-22 19:38:09 +0530

    shell-script

--- 21-backup.sh
+++ 21-backup.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-SOURCE_DIR="$1"
-DESTINATION_DIR="$2"
 TIMESTAMP=$(date +F%-%H-%M-%S)
 
 echo "Please enter the source directory to create back: "                 
@@ -9,9 +7,9 @@ read Source directory
 echo "Please enter the destination directory to save .tar.gz file: "    
 read Destination directory                                                                                        
                                                     
-folder=$(echo "$SOURCE_DIR"| awk -f / '{print $NF}')                                    
+folder=$(echo "$SD"| awk -F / '{print $NF}')                                    
 gzfile="$folder-$TIMESTAMP.tar.gz"                                            
-cd $SOURCE_DIR
+cd $SD
 cd ..
 tar -r -f "$folder-$TIMESTAMP.tar.gz" $folder                                         
-mv $gzfile $DESTINATION_DIR
+mv $gzfile $DD
