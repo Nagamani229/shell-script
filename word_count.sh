9cca29e479474260111950c5c159236329c91f8d
Potu Nagamani <Nagamani.X.Potu@kp.org>
2024-12-22 14:26:33 +0530

    shell-script

--- word_count.sh
+++ word_count.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-  # Check if a file is provided as an argument
-if [ -z "$1" ]; then
-    echo "Please provide a text file as an argument."
-    exit 1
+FILE_NAME="passwd"
+
+fi [ ! -f $FILE_NAME ];then
+  echo "File $FILE_NAME not found"
+  exit 1
 fi
 
-# Read the input file, process it to count word frequencies, and show the top 5 most frequent words
-cat "$1" | tr -cs '[:alpha:]' '[\n*]' | tr '[:upper:]' '[:lower:]' | sort | uniq -c | sort -nr | head -n 5
+# Read the file, normalize text to lowercase, remove punctuation, and count occurrences
+word_count=$(tr -s '[:space:]' '\n' < "$file_name" | tr -d '[:punct:]' | tr '[:upper:]' '[:lower:]' | grep -v '^$' | sort | uniq -c | sort -nr)
+
+# Display the top 5 most frequent words
+echo "Top 5 Most Frequent Words:"
+echo "$word_count" | head -n 5 | awk '{print $2 " - " $1 " occurrences"}'
