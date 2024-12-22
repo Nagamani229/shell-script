d0426cdc459180f2e5a70583e132f1feefebdf2d
Potu Nagamani <Nagamani.X.Potu@kp.org>
2024-12-22 19:34:27 +0530

    shell-script

--- 21-backup.sh
+++ 21-backup.sh
@@ -4,14 +4,14 @@ SOURCE_DIR="$1"
 DESTINATION_DIR="$2"
 TIMESTAMP=$(date +F%-%H-%M-%S)
 
-echo "Please enter the source directory to create back: "                 #print message
-read SD                                                                                              #Read input from user                                      
-echo "Please enter the destination directory to save .tar.gz file: "    #print message
-read DD                                                                                             #Read input from user
+echo "Please enter the source directory to create back: "                 
+read Source directory                                                                                                                                  
+echo "Please enter the destination directory to save .tar.gz file: "    
+read Destination directory                                                                                        
                                                     
-folder=$(echo "$SD"| awk -F / '{print $NF}')                                    # to get folder name from absolute path
-gzfile="$folder-$TIME.tar.gz"                                            
-cd $SD
+folder=$(echo "$SOURCE_DIR"| awk -F / '{print $NF}')                                    
+gzfile="$folder-$TIMESTAMP.tar.gz"                                            
+cd $SOURCE_DIR
 cd ..
-tar -r -f "$folder-$TIME.tar.gz" $folder                                            # create .tar.gz file
-mv $gzfile $DD
+tar -r -f "$folder-$TIMESTAMP.tar.gz" $folder                                         
+mv $gzfile $DESTINATION_DIR
